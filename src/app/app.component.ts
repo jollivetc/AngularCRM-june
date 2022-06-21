341a653248c52e2e236070f4876f0cb869739adc
Christophe Jollivet <jollivet@apside.fr>
2022-06-21 10:40:04 +0200

    start of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularCRM';
+  title = 'Apside';
+  name:string= 'Bob';
+
+  fruits:string[]=['apple', 'pear','banana'];
+
+
+  clicked($event:MouseEvent):void{
+    this.name = "John";
+    console.log($event);
+  }
 }
