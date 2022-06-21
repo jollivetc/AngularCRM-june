26a110befbb2039f31e106ba8c9da0db79802d0a
Christophe Jollivet <jollivet@apside.fr>
2022-06-21 17:26:50 +0200

    beginning of guard implementation

--- src/app/login/authentication.service.ts
+++ src/app/login/authentication.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
+import { User } from './model/user';
+
+const USER_TOKEN_KEY = 'angularCRM.USER';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private user?:User;
+
+  constructor() {
+    if(sessionStorage.getItem(USER_TOKEN_KEY)){
+      this.user= JSON.parse(sessionStorage.getItem(USER_TOKEN_KEY)!);
+    }
+   }
 
   authentUser(login: string, password:string){
-    return {
-      userId: 1,
+
+    this.user =  {
+      id: 1,
       login: login,
       lastname: 'Doe',
       firstname: 'John'
     }
+    sessionStorage.setItem(USER_TOKEN_KEY, JSON.stringify(this.user))
+    return this.user;
   }
 }
