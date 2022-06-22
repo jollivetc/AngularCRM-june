6986aa54173b4c83b1036960900a16573600c013
Christophe Jollivet <jollivet@apside.fr>
2022-06-22 10:53:10 +0200

    observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscriber } from 'rxjs';
+import { DemoObservableService } from '../common/demo-observable.service';
 
 @Component({
   selector: 'crm-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private demoObservable: DemoObservableService) { }
 
   ngOnInit(): void {
   }
 
+  callObservable(){
+    const subscriber = {
+      next:(value:number)=>{console.log(value)},
+      error: (error:Error)=>{console.error(error)},
+      complete:()=>{console.log('finished')}
+    }
+    this.demoObservable.testObservable().subscribe(subscriber);
+  }
 }
